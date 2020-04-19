2a01e0f0966eaa4f4b6e844eef71b21eb247624f
卞小君 <xiaojun.bian@mihoyo.com>
2020-04-19 13:10:03 +0800

    更新接口

--- babysmile/Assets/Scripts/BabySmileManager.cs
+++ babysmile/Assets/Scripts/BabySmileManager.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using LD46;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class BabySmileManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -16,79 +17,48 @@ public class GameManager : MonoBehaviour
         
     }
 
-    //元指令数组：每一项都是Tuple<int,int>类型，第一个int代表指令类型，第二个是指令参数
+    private static List<int> inTableList = new List<int>();
 
-    //指令类型：指令参数
-    //0: 空指令
-    //1：拾取物品：物品ID 
-    //2：拾取新产生的物品：物品Type
-    //3：将手里物品放到桌子上：桌子ID
-    //4：将手里物品放置在地上 
-    //5：手里物品消失 
-    //6：物品切换到下一状态 物品ID
-    //7：开始计时条时间 计时条Type
-    //8：婴儿健康值变化 新值
-    //9：婴儿快乐值变化 新值
-    //10：钱包值变化 新值
+    private static Dictionary<Tuple<int, int>, OrderSet> itemOrderMapping = new Dictionary<Tuple<int, int>, OrderSet>();
 
-
-    // 当前table上的物品序号，没有物品为0
-    List<int> inTableList = new List<int>();
-    Dictionary<Tuple<int, int>, OrderSet> itemOrderMapping = new Dictionary<Tuple<int, int>, OrderSet>();
-
-    List<Tuple<int, int>> getOrderList(int inTableID, int inHandID)
+    class OrderSet
     {
-        Tuple<int, int> key = new Tuple<int, int>(inHandID, inTableID);
-        if (itemOrderMapping.ContainsKey(key))
+        List<Tuple<int, int>> orderList;
+
+        public List<Tuple<int, int>> Order
         {
-            return itemOrderMapping[key].Order;
+            get => orderList;
         }
-        else
-        {
-            return new List<Tuple<int, int>>();
-        }
+
     }
 
-    List<Tuple<int,int>> OperationCheck(int tableID, int inHandID)
-    {
-        List<Tuple<int, int>> orderSet = new List<Tuple<int, int>>();
-        int inTableID = inTableList[tableID];
-        if(inTableID == 0 && inHandID == 0)
-        {
-            //桌子空，手里空 没操作
-        }
-        else if(inTableID == 0 && inHandID > 0)
-        {
-            //桌子空，手里有 将东西放到桌子上
-            orderSet.Add(new Tuple<int, int>(3, tableID));
-        }
-        else if(inTableID > 0 && inHandID == 0)
-        {
-            //桌子有，手里空：拾取东西到手里
-            orderSet.Add(new Tuple<int, int>(1, inTableID));
-        }
-        else if(inTableID > 0 && inHandID > 0)
-        {
-            //桌子有，手里有：将手里东西施加到桌子上东西上，返回施加结果
-            orderSet.AddRange(getOrderList(inTableID, inHandID));
-        }
-        return orderSet;
-    }
 
-    void ExecuteOrder(Tuple<int, int> item)
+
+    private void executeOrder(Tuple<int, int> item)
     {
 
     }
+
+    public static int GetTableItemID(int tableid)
+    {
+        return 0;
+    }
+
+    public static int GetItemState(int itemid)
+    {
+        return 0;
+    }
+
+    public static List<Tuple<int, int>> InteractItem(int itemid)
+    {
+        return new List<Tuple<int, int>>();
+    }
+
+    public static List<Tuple<int, int>> InteractTable(int tableid)
+    {
+        return new List<Tuple<int, int>>();
+    }
+
 
 }
 
-class OrderSet
-{
-    List<Tuple<int, int>> orderList;
-
-    public List<Tuple<int, int>> Order
-    {
-        get => orderList;
-    }
-
-}
