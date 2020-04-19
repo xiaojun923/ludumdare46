5d721478a4a8463660788932dce34c5c17a26639
卞小君 <xiaojun.bian@mihoyo.com>
2020-04-19 23:28:00 +0800

    主脚本1.0

---
+++ babysmile/Assets/Scripts/BabyActor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabyActor : MonoBehaviour
+{
+    public SceneItem item;
+    public static List<int> taskList = new List<int>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        item = GetComponent<SceneItem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
