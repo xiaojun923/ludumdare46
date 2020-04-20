712eb7ef72fc0bb0a7c9fad17c4659edb099950a
卞小君 <xiaojun.bian@mihoyo.com>
2020-04-21 01:10:30 +0800

    花盆逻辑修正

--- babysmile/Assets/Scripts/FlowerActor.cs
+++ babysmile/Assets/Scripts/FlowerActor.cs
@@ -18,19 +18,20 @@ public class FlowerActor : MonoBehaviour
         int flowerStat = BabySmileManager.GetItemState(itemid);
         int placeid = BabySmileManager.GetTableType(BabySmileManager.GetItemTableID(itemid));
 
+        /*
         if (flowerStat == 2 || flowerStat == 4 || flowerStat == 5)
         {
             //枯苗和枯花和成花都是锁定在阳台上不可拾取的，只能对其进行浇水或者采摘操作
-            BabySmileManager.SetPickable(itemid, false);
-        }
-        else if (placeid == 3 && (flowerStat == 1 || flowerStat == 3))
+            //BabySmileManager.SetPickable(itemid, false);
+        }*/
+        if (placeid == 3 && (flowerStat == 1 || flowerStat == 3))
         {
             //生苗和生花可以拾取，但只有在阳台才会生长
             acumTime += Time.deltaTime;
             if (acumTime > waterTime)
             {
                 acumTime = 0;
-                BabySmileManager.SetPickable(itemid, true);
+                //BabySmileManager.SetPickable(itemid, true);
                 BabySmileManager.SetItemState(itemid, flowerStat + 1);
                 //表现层改状态为枯苗/枯花
             }
